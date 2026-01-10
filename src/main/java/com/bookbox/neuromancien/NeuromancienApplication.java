24943a55c390ed31667d97aa8cec14146c8bf7e1
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2026-01-10 01:06:59 +0100

    build(init): initialize project

---
+++ src/main/java/com/bookbox/neuromancien/NeuromancienApplication.java
@@ -0,0 +1,13 @@
+package com.bookbox.neuromancien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuromancienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuromancienApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookbox/neuromancien/NeuromancienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookbox.neuromancien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuromancienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
