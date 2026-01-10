75cfda5898dda83193fc592c861ecaef39f48d86
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2026-01-10 22:20:45 +0100

    feat: add user model and repository

---
+++ src/main/java/com/bookbox/neuromancien/auth/model/User.java
@@ -0,0 +1,39 @@
+package com.bookbox.neuromancien.auth.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
+
+    @Indexed(unique = true)
+    private String username;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
